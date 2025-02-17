c9e81204ab3c06c5051ffaf6e2add2db2c6f9d82
Souradeep321 <souradeephazra93@gmail.com>
2025-02-18 02:01:55 +0530

    Env variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
-function App() {
+import conf from './conf/conf'
 
+function App() {
+  console.log(conf.appWriteUrl);
+  
   return (
     <h1 className="text-4xl font-bold underline text-center bg-pink-400">
       Hello world!
