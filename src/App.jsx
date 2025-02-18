08f6aa4e5abe70c1721416725bb48653dc11202d
Souradeep321 <souradeephazra93@gmail.com>
2025-02-18 22:17:45 +0530

    Authentication appwrite services done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import conf from './conf/conf'
-
 function App() {
-  console.log(conf.appWriteUrl);
   
   return (
     <h1 className="text-4xl font-bold underline text-center bg-pink-400">
--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,5 +1,5 @@
 const conf = {
-    appWriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
     appProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     appDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
