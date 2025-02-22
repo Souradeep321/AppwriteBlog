81dc235a7947e141ccf3856b5bfa314a3496eee7
Souradeep321 <souradeephazra93@gmail.com>
2025-02-22 10:45:10 +0530

    Redux Store Setup Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,55 @@
+import './App.css'
+import { useState, useEffect } from "react"
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { Header } from './components/index'
+import { Footer } from './components/index'
+import { Outlet } from 'react-router-dom'
+
 function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  // dispatch(login(userData)) Try this if {userData} didn't work
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching user:", error);
+        dispatch(logout())
+      })
+      .finally(() => setLoading(false))
+  }, [])
   
-  return (
-    <h1 className="text-4xl font-bold underline text-center bg-pink-400">
-      Hello world!
-    </h1>
-  )
+  // if (loading) {
+  //   return <h1 className="text-2xl text-center text-gray-600">Loading...</h1>;
+  // }
+
+  // return (
+  //   <h1 className="text-4xl font-bold underline text-center bg-pink-400">
+  //     Hello world!
+  //   </h1>
+  // );
+
+  return !loading ?
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          TODO: {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+    : <h1 className="text-2xl text-center text-gray-600">Loading...</h1>;
 }
+
 
 export default App
